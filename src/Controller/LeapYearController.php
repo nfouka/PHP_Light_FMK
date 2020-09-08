b8189c8544685a68f927cb237171ffbcb57e18a3
nadir.fouka@cea.fr <nadir.fouka@gmail.com>
2020-09-08 23:07:47 +0200

    Add model , create the core Framework
    Simplify the routing

--- src/Controller/LeapYearController.php
+++ src/Controller/LeapYearController.php
@@ -5,13 +5,17 @@ namespace Controller ;
 
 use Symfony\Component\HttpFoundation\Response;
 use Core\AbstractController ; 
-use  Symfony\Component\HttpFoundation\Request; 
+use Symfony\Component\HttpFoundation\Request; 
+use Model\Yeap ; 
 
 class LeapYearController extends AbstractController
-{
+{  
     public function index($param1 , Request $request )
     {
-        if (is_leap_year($param1)) {
+
+        $model = new Yeap()  ; 
+
+        if (  $model->is_leap_year($param1)) {
             ob_start();
             $request->attributes->set('is_year', 'Yep, '.$param1.' This is a leap year! ');
             include sprintf(__DIR__.'/../pages/%s.php', 'leap_year' );
@@ -23,6 +27,21 @@ class LeapYearController extends AbstractController
             return new Response(ob_get_clean());
         }
 
-       
+    }
+
+    public function hello($name,Request $request){
+        $request->attributes->set('name', $name );
+        include sprintf(__DIR__.'/../pages/%s.php', 'hello' );
+        return new Response(ob_get_clean());
+    }
+
+    public function bye(){
+        include sprintf(__DIR__.'/../pages/%s.php', 'bye' );
+        return new Response(ob_get_clean());
+    }
+
+    public function home(){
+        include sprintf(__DIR__.'/../pages/%s.php', 'home' );
+        return new Response(ob_get_clean());
     }
 }
--- src/routing.php
+++ src/routing.php
@@ -4,24 +4,25 @@
 // example.com/src/app.php
 use Symfony\Component\Routing;
 
-function is_leap_year($year = null) {
-    if (null === $year) {
-        $year = date('Y');
-    }
-    return 0 === $year % 400 || (0 === $year % 4 && 0 !== $year % 100);
-}
-
 $routes =             new Routing\RouteCollection();
-$routes->add('hello', new Routing\Route('/hello/{name}', ['name' => 'World']));
-$routes->add('bye',   new Routing\Route('/bye'));
-$routes->add('index',   new Routing\Route('/index'));
-
 
 $routes->add('leap_year', new Routing\Route('/is_leap_year/{param1}', [
     'param1' => null, 
     '_controller' => 'Controller\LeapYearController::index',
 ]));
 
+$routes->add('hello', new Routing\Route('/hello/{name}', [
+    'name' => null , 
+    '_controller' => 'Controller\LeapYearController::hello',
+]));
+
+$routes->add('bye', new Routing\Route('/bye', [
+    '_controller' => 'Controller\LeapYearController::bye',
+]));
+
+$routes->add('home', new Routing\Route('/home', [
+    '_controller' => 'Controller\LeapYearController::home',
+]));
 
 return $routes;
 

5d095783b738a95f6d6a29a197d7d40a61e5e1b9
nadir.fouka@cea.fr <nadir.fouka@gmail.com>
2020-09-10 15:47:24 +0200

    fix warning missed files ( css,js )

--- src/pages/home.php
+++ src/pages/home.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+    <link rel="icon" href="<?php echo '/assets/img/favicon/favicon.ico' ?>" >
 
     <title>Cover Template for Bootstrap</title>
 
@@ -57,6 +57,6 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
     <script src="<?php echo '/assets/js/js.js' ?>"></script>
-    <script src="../../dist/js/bootstrap.min.js"></script>
+    <script src="<?php echo '/assets/js/bootstrap.min.js'?>"></script>
   </body>
 </html>

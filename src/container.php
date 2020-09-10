6b324fac913e804208e2a32155acb28670bc437f
nadir.fouka@cea.fr <nadir.fouka@gmail.com>
2020-09-10 21:37:40 +0200

    add container DependencyInjection design patern

---
+++ src/container.php
@@ -0,0 +1,49 @@
+<?php 
+
+
+
+use Core\Framework;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
+use Symfony\Component\HttpKernel\Controller\ControllerResolver;
+use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+use Event\ResponseEvent;
+use Listenner\MyListenner1;
+
+
+$containerBuilder = new ContainerBuilder();
+$containerBuilder->register('context', 'Symfony\Component\Routing\RequestContext');
+$containerBuilder->register('matcher', 'Symfony\Component\Routing\Matcher\UrlMatcher')
+        ->setArguments([$routes, new Reference('context')]) ;
+
+$containerBuilder->register('resolver', ControllerResolver::class );
+$containerBuilder->register('argumentResolver',  ArgumentResolver::class );
+
+
+$containerBuilder->register( ResponseEvent::NAME , \Listenner\SubscriberEvenetDispatcher::class ) ; 
+$containerBuilder->register('dispatcher', EventDispatcher::class )
+
+    ->addMethodCall('addSubscriber', [new Reference(ResponseEvent::NAME)]) ; 
+
+
+    
+$containerBuilder->register('framework', Framework::class )
+->setArguments(array(
+    new Reference('dispatcher'),
+    new Reference('matcher'),
+    new Reference('resolver') ,
+    new Reference('argumentResolver')
+)) ; 
+
+return $containerBuilder  ; 
+
+?>
+
+/*
+
+$dispatcher = new EventDispatcher();
+$dispatcher->addListener( ResponseEvent::NAME , [new MyListenner1() , 'onResponse'], 1 );
+$dispatcher->addListener( ResponseEvent::NAME , [new MyListenner2() , 'onResponse'] ,0 );
+
+*/

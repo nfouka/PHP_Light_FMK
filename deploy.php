4f150e9235870749ab686b74de105cd256855ec2
Nadir Fouka <nfouka@users.noreply.github.com>
2020-09-19 11:48:19 +0200

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -30,7 +30,7 @@ host('127.0.0.1')
     ->set('deploy_path', '/var/www/html/develop'); 
 
 
-host('192.168.1.33')
+host('176.149.71.189')
     ->stage('production')
     ->user('nadir')
     ->identityFile('~/.ssh/id_rsa')

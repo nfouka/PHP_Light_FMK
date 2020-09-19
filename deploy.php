b2f01bc3c0bbcd0a10bb4464cbcf9b35d8a88a87
Nadir Fouka <nfouka@users.noreply.github.com>
2020-09-19 12:24:56 +0200

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -7,7 +7,7 @@ require 'recipe/common.php';
 set('application', 'var/www/html');
 
 // Project repository
-set('repository', 'https://github.com/nfouka/PHP_Light_FMK');
+set('repository', 'https://github.com/nfouka/git-tuto');
 
 // [Optional] Allocate tty for git clone. Default value is false.
 // set('git_tty', true); 
